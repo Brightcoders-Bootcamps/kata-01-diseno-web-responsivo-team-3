9b2bda57dd1dfe89e493c9e99e8afdcfe742b0d9
Dhyana Ixchel Verjan Vargas <dvvargas@ucol.mx>
2020-09-10 18:09:49 -0500

    Create js file
    Functions and exceptions were created to get the shortlink
    from the API and the objects of the form from the
    index  were modified adding the id and changing the type of them.

---
+++ functions/main.js
@@ -0,0 +1,59 @@
+const form = document.getElementById('linkForm');
+form.addEventListener("submit", function(event){
+  event.preventDefault();
+
+  let longUrl = event.target.elements['longUrl'].value;
+  
+  fetchNewLink(longUrl);
+});
+
+
+
+
+async function fetchNewLink(longUrl) {
+  try {
+    let newLinkJson = await postLink(longUrl);
+    let newLink = await getShortLink(newLinkJson);
+
+    console.log(newLink);
+} catch(urlError) {
+    console.error('Fatal error: ', urlError);
+    /* function showError(){
+        alert ("An error with your link has ocurred.);
+        } */
+}
+}
+
+function postLink(longUrl) {
+  return fetch('https://rel.ink/api/links/', {
+      method: 'POST',
+      body: JSON.stringify({
+        url: longUrl
+      }),
+      headers: {
+        "Content-type": "application/json"
+      }
+    })
+    .then(response => {
+      if(response.ok) {
+        return response.json();
+      }
+      
+      throw new Error(`postLink failed due to ${response.statusText}`);
+    });
+}
+
+function getShortLink(response) {
+  return fetch('https://rel.ink/api/links/' + response.hashid) // https://rel.ink/api/links/hola
+    .then(result => {
+      if (result.ok){
+        return result.json();
+      }
+      throw new Error(`getShortLink failed due to ${result.statusText}`);
+  });
+}
+
+let contenido = document.querySelector('#contenido')
+//function getLink(element){
+//	alert(element);
+//}
